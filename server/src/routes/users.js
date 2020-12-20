c485488b9e3d5702010035340f828a9ef0e8d35e
Kiera Carman <kiera.carman@gmail.com>
2020-12-20 12:31:37 -0500

    Move routes logic to controller file

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -1,106 +1,17 @@
-const express = require('express');
+const { Router } = require('express');
 
-const router = express.Router();
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-require('dotenv').config();
+const router = Router();
 
-// Load input validation
-const validateRegisterInput = require('../validation/register');
-const validateLoginInput = require('../validation/login');
-
-// Load User model
-const User = require('../models/user');
+const usersController = require('../controllers/users');
 
 // @route POST api/users/register
 // @desc Register user
 // @access Public
-router.post('/register', (req, res) => {
-  // Form validation
-  const { errors, isValid } = validateRegisterInput(req.body);
-
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  User.findOne({ email: req.body.email }).then((user) => {
-    if (user) {
-      return res.status(400).json({ email: 'Email already exists.' });
-    }
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
-
-    // Hash password before saving in database
-    bcrypt.genSalt(10, (error, salt) => {
-      bcrypt.hash(newUser.password, salt, (err, hash) => {
-        if (err) throw err;
-        newUser.password = hash;
-        newUser
-          .save()
-          .then((result) => res.json(result))
-          .catch((e) => console.log(e));
-      });
-    });
-  });
-});
+router.post('/register', usersController.register);
 
 // @route POST api/users/login
 // @desc Login user and return JWT token
 // @access Public
-router.post('/login', (req, res) => {
-  // Form validation
-  const { errors, isValid } = validateLoginInput(req.body);
-
-  // Check validation
-  if (!isValid) {
-    return res.status(400).json(errors);
-  }
-
-  const { email } = req.body;
-  const { password } = req.body;
-
-  // Find user by email
-  User.findOne({ email }).then((user) => {
-    // Check if user exists
-    if (!user) {
-      return res.status(404).json({ emailNotFound: 'Email not found.' });
-    }
-
-    // Check password
-    bcrypt.compare(password, user.password).then((isMatch) => {
-      if (isMatch) {
-        // User matched
-        // Create JWT Payload
-        const payload = {
-          id: user.id,
-          name: user.name,
-        };
-
-        // Sign token
-        jwt.sign(
-          payload,
-          process.env.SECRET_OR_KEY,
-          {
-            expiresIn: 31556926, // 1 year in seconds
-          },
-          (err, token) => {
-            res.json({
-              success: true,
-              token: `Bearer ${token}`,
-            });
-          },
-        );
-      } else {
-        return res
-          .status(400)
-          .json({ incorrectPassword: 'Incorrect password.' });
-      }
-    });
-  });
-});
+router.post('/login', usersController.login);
 
 module.exports = router;
