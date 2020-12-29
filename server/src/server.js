fd28571d71f8be41f39364205e690b7edf17a44d
Kiera Carman <kiera.carman@gmail.com>
2020-12-29 18:17:41 -0500

    Change mongodb env variable name

--- server/src/server.js
+++ server/src/server.js
@@ -21,17 +21,17 @@ app.use(bodyParser.json());
 
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
-  app.use(express.static('client/build/'));
+  app.use(express.static('../../client/build/'));
 
   app.get('/*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build/'));
+    res.sendFile(path.resolve(__dirname, '../../client/build/'));
   });
 }
 
 // Initialize connection once and create connection pool
 mongoose
   .connect(
-    process.env.DATABASE_URL, {
+    process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     },
