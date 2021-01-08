fdcb38d9aec29f1a7fc0bb282df4cb9781b25523
Kiera Carman <kiera.carman@gmail.com>
2021-01-08 15:25:50 -0500

    Fix serving of index.html so React Router works

--- server.js
+++ server.js
@@ -31,10 +31,9 @@ if (process.env.NODE_ENV === 'production') {
   });
 
   // Serve any static files
-  app.use(express.static('client/build'));
-
+  app.use(express.static(path.join(__dirname, 'client/build')));
   app.get(/^\/(?!api).*/, (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client/build'));
+    res.sendFile('index.html', { root: path.join(__dirname, 'client/build') });
   });
 }
 
