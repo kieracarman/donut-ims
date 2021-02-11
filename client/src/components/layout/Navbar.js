3635a66f306c594e7392fb990d16998c83db4dc7
Kiera Carman <kiera.carman@gmail.com>
2021-02-11 14:03:03 -0500

    Change navbar to sidebar design

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -14,29 +14,27 @@ class Navbar extends Component {
 
   render() {
     return (
-      <div className='navbar-fixed'>
-        <nav className="navbar navbar-expand-sm navbar-dark bg-dark" style={{marginBottom: "20px"}}>
-          <Link to="/" className="navbar-brand"><img src={logo} width="30" height="30" alt="Logo" /></Link>
-          <Link to="/" className="navbar-brand">One Ring Donuts IMS</Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav mr-auto">
-             <li className="navbar-item">
-                <Link to="/" className="nav-link">Inventory</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/manage" className="nav-link">Manage Items</Link>
-              </li>
-              <li className="navbar-item">
-                <Link to="/order" className="nav-link">Order Restock</Link>
-              </li>
-            </ul>
-            <ul className="navbar-nav navbar-right">
-              <li className="navbar-item">
-                <Link to='/logout' className="nav-link" onClick={this.onLogoutClick}>Logout</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
+      <div className='cd-navbar bg-dark'>
+        <Link to='/'><img src={logo} width='30' height='30' alt='Logo' /></Link>
+        <Link to='/'>One Ring Donuts IMS</Link>
+        <div>
+          <ul>
+            <li>
+              <Link to='/'>Inventory</Link>
+            </li>
+            <li>
+              <Link to='/manage'>Manage Items</Link>
+            </li>
+            <li>
+              <Link to='/order'>Order Restock</Link>
+            </li>
+          </ul>
+          <ul>
+            <li>
+              <Link to='/logout' onClick={this.onLogoutClick}>Logout</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     );
   }
