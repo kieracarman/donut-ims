ca8da51d72014756307b5bfc0dbf619ab2853b9e
Kiera Carman <kiera.carman@gmail.com>
2021-02-11 23:32:13 -0500

    Add icons and completely restyle UI

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { logoutUser } from '../../actions/authActions';
+import { List, Edit, Truck, LogOut } from 'react-feather';
 
-import logo from '../../Logo.png';
+import logo from '../../oneringlogo.svg';
 
 class Navbar extends Component {
   onLogoutClick = e => {
@@ -15,29 +16,27 @@ class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className='cd-navbar bg-dark'>
+        <nav className='cd-navbar'>
           <div className='cd-navbar-brand'>
-            <Link to='/'><img src={logo} width='100%' alt='Logo' /></Link>
-            <Link to='/'>IMS</Link>
+            <NavLink to='/'><img src={logo} width='100%' alt='Logo' /></NavLink>
+            <NavLink to='/'>IMS</NavLink>
           </div>
-          <div>
-            <ul className='cd-navbar-list'>
-              <li className='cd-navbar-item'>
-                <Link to='/' className='cd-navbar-link'>Inventory</Link>
-              </li>
-              <li className='cd-navbar-item'>
-                <Link to='/manage' className='cd-navbar-link'>Manage Items</Link>
-              </li>
-              <li className='cd-navbar-item'>
-                <Link to='/order' className='cd-navbar-link'>Order Restock</Link>
-              </li>
-            </ul>
-            <ul className='cd-navbar-footer'>
-              <li className='cd-navbar-item'>
-                <Link to='/logout' className='cd-navbar-link' onClick={this.onLogoutClick}>Logout</Link>
-              </li>
-            </ul>
-          </div>
+          <ul className='cd-navbar-list'>
+            <li className='cd-navbar-item'>
+              <NavLink exact to='/' className='cd-navbar-link' activeClassName='cd-navbar-link-active'><List className='cd-navbar-link-icon' /><div className='cd-navbar-link-text'>Inventory</div></NavLink>
+            </li>
+            <li className='cd-navbar-item'>
+              <NavLink to='/manage' className='cd-navbar-link' activeClassName='cd-navbar-link-active'><Edit className='cd-navbar-link-icon' /><div className='cd-navbar-link-text'>Manage Items</div></NavLink>
+            </li>
+            <li className='cd-navbar-item'>
+              <NavLink to='/order' className='cd-navbar-link' activeClassName='cd-navbar-link-active'><Truck className='cd-navbar-link-icon' /><div className='cd-navbar-link-text'>Order Restock</div></NavLink>
+            </li>
+          </ul>
+          <ul className='cd-navbar-footer'>
+            <li className='cd-navbar-item'>
+              <NavLink to='/logout' className='cd-navbar-link' onClick={this.onLogoutClick}><LogOut className='cd-navbar-link-icon' /><div className='cd-navbar-link-text'>Logout</div></NavLink>
+            </li>
+          </ul>
         </nav>
       </div>
     );
